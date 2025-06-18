2886b1eaabc744090058dfc536662cfeefd5b718
Skyler110120 <skyleryarbroughj04@gmail.com>
2025-06-18 10:42:30 -0500

    First Commit: set up react native app with Expo Router

---
+++ app/index.tsx
@@ -0,0 +1,19 @@
+import { Text, View, Button } from "react-native";
+import { Link } from "expo-router";
+
+export default function Index() {
+  return (
+    <View
+      style={{
+        flex: 1,
+        justifyContent: "center",
+        alignItems: "center",
+      }}
+    >
+      <Text>Stone Cold Tactical Home Screen</Text>
+      <Link href="/screens/Login" asChild>
+        <Button title="Go to Login" />
+      </Link>
+    </View>
+  );
+}
